4f53848becd79ff13cf9b0af6ddba90605b56190
thay <91734800+fmkr-project@users.noreply.github.com>
2023-11-02 18:15:31 +0100

    Update class diagram

---
+++ src/deck.py
@@ -0,0 +1,8 @@
+class Deck:
+    """
+    Class representing a set of cards
+    """
+    def __init__(self):
+        self.id = None      # TODO
+        self.name = ""      # TODO
+        self.contents = []  # TODO discuss about the structure (dict ? (true) set ? list ?)
