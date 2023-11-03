fcdfea915360ae29cb5556a4874dd8395341c2c5
thay <91734800+fmkr-project@users.noreply.github.com>
2023-11-03 18:35:45 +0100

    Main file template

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Main entry point"""
+
+import deck
+
+import os
+
+if __name__ == '__main__' :
+    # Use root directory instead of src
+    os.chdir("..")
+
+    # TODO put some sqlite here
+
+    # Put here the main loop code
