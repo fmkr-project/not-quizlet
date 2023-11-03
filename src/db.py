dfef8a5aee07f94b40e9c34b51e0de92e21b575e
thay <91734800+fmkr-project@users.noreply.github.com>
2023-11-03 19:40:21 +0100

    Refactor

--- src/db.py
+++ src/db.py
@@ -8,12 +8,12 @@ class Database:
         self.db_path = db_path
     
     def connect(self):
-        self.database = sql.connect(self.db_path)
+        self.conn = sql.connect(self.db_path)
 
     def execute_sql(self, query):
-        return self.database.cursor().executescript(query)
+        return self.conn.cursor().executescript(query)
     
 
     def create_deck(self, id, name):
-        self.database.cursor().execute("""insert into decks (id, name) values (?, ?)""", (id, name))
-        self.database.commit()
+        self.conn.cursor().execute("""insert into decks (id, name) values (?, ?)""", (id, name))
+        self.conn.commit()
