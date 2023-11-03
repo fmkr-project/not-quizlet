abfe881c65afa7e049ae1bbaa52ff68ee15ff4fd
thay <91734800+fmkr-project@users.noreply.github.com>
2023-11-03 19:38:47 +0100

    Move connect function in db.py

--- src/main.py
+++ src/main.py
@@ -7,17 +7,14 @@ import deck
 import db
 
 import sqlite3 as sql
-import os
-
-DATABASE_LOCATION = "res/data.db"
 
 if __name__ == '__main__' :
     # Create the db if it does not exist
     with open('res/create_database.sql') as sqlfile:
         script = sqlfile.read()
     
-    data = db.Database(sql.connect(DATABASE_LOCATION))
-
+    data = db.Database()
+    data.connect()
     data.execute_sql(script)
 
     # Put here the main loop code
