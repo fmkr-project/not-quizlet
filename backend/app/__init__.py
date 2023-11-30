d015f7369d6be1c752f29c10e1b0654ca8a01757
Skanx7 <skandergorgi007@gmail.com>
2023-11-30 10:51:22 +0100

    fix fking api
    api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,3 @@
 from .app import create_app
-
-#def create_app(config_filename = "config/development.py") -> Flask:
-    #app : Flask = Flask(__name__)
-    #app.config.from_pyfile(config_filename)
-    #app.register_blueprint(api_blueprint)
-    #return app
+from .app import print_routes
 my_app = create_app()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 """Main entry point"""
 import env
 from db import my_db, run_tests
-from app import my_app  
+from app import my_app, print_routes 
 from os import *
 
 API_VERSION = getenv("API_VERSION")      
@@ -9,7 +9,8 @@ if __name__ == '__main__' :
     #my_db.reset()
     #my_db.execute_sql()
     #run_tests()
-    my_app.run()
+    my_app.run(port=5001)
+    #print_routes(my_app)
     #result = my_db.execute_query("SELECT id, username FROM users", data_manip=False)
     #print(result)
     
