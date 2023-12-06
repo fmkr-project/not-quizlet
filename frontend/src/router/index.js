d42e0621d04ae673c4f1dcea3841255f06402257
rmnouv <rathanakmuny2nouv@gmail.com>
2023-12-06 22:25:12 +0100

    Change the connect of main page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -60,170 +60,170 @@ const defaultChildRoutes = (prefix) => [
     meta: { auth: true, name: 'Home', isBanner: true },
     component: () => import('@/views/dashboards/IndexPage.vue')
   },
-//   // Spacial Pages
-//   {
-//     path: '/billing',
-//     name: prefix + '.billing',
-//     meta: { auth: true, name: 'Billing', isBanner: true },
-//     component: () => import('@/views/spacial-pages/BillingPage.vue')
-//   },
-//   {
-//     path: '/calender',
-//     name: prefix + '.calender',
-//     meta: { auth: true, name: 'Calender', isBanner: true },
-//     component: () => import('@/views/spacial-pages/CalenderPage.vue')
-//   },
-//   {
-//     path: '/kanban',
-//     name: prefix + '.kanban',
-//     meta: { auth: true, name: 'Kanban', isBanner: true },
-//     component: () => import('@/views/spacial-pages/KanbanPage.vue')
-//   },
-//   {
-//     path: '/pricing',
-//     name: prefix + '.pricing',
-//     meta: { auth: true, name: 'Pricing', isBanner: true },
-//     component: () => import('@/views/spacial-pages/PricingPage.vue')
-//   },
-//   {
-//     path: '/timeline',
-//     name: prefix + '.timeline',
-//     meta: { auth: true, name: 'Timeline', isBanner: true },
-//     component: () => import('@/views/spacial-pages/TimelinePage.vue')
-//   },
-//   {
-//     path: '/rtl-support',
-//     name: prefix + '.rtlsupport',
-//     meta: { auth: true, name: 'RTL-Support', isBanner: true },
-//     component: () => import('@/views/spacial-pages/RtlSupport.vue')
-//   },
-//   // Users Pages
-//   {
-//     path: '/user-list',
-//     name: prefix + '.user-list',
-//     meta: { auth: true, name: 'User List', isBanner: true },
-//     component: () => import('@/views/user/ListPage.vue')
-//   },
-//   {
-//     path: '/user-add',
-//     name: prefix + '.user-add',
-//     meta: { auth: true, name: 'User Add', isBanner: true },
-//     component: () => import('@/views/user/AddPage.vue')
-//   },
-//   {
-//     path: '/user-profile',
-//     name: prefix + '.user-profile',
-//     meta: { auth: true, name: 'User Add', isBanner: true },
-//     component: () => import('@/views/user/ProfilePage.vue')
-//   },
-//   {
-//     path: '/privacy-setting',
-//     name: prefix + '.user-privacy-setting',
-//     meta: { auth: true, name: 'Privacy Setting', isBanner: true },
-//     component: () => import('@/views/user/PrivacySetting.vue')
-//   },
-//   // Widgets Pages
-//   {
-//     path: '/widget-basic',
-//     name: prefix + '.widget-basic',
-//     meta: { auth: true, name: 'Widget Basic', isBanner: true },
-//     component: () => import('@/views/widgets/WidgetBasic.vue')
-//   },
-//   {
-//     path: '/widget-chart',
-//     name: prefix + '.widget-chart',
-//     meta: { auth: true, name: 'Widget Chart', isBanner: true },
-//     component: () => import('@/views/widgets/WidgetChart.vue')
-//   },
-//   {
-//     path: '/widget-card',
-//     name: prefix + '.widget-card',
-//     meta: { auth: true, name: 'Widget Card', isBanner: true },
-//     component: () => import('@/views/widgets/WidgetCard.vue')
-//   },
-//   // Map Pages
-//   {
-//     path: '/map-google',
-//     name: prefix + '.map-google',
-//     meta: { auth: true, name: 'Google Map', isBanner: true },
-//     component: () => import('@/views/maps/GooglePage.vue')
-//   },
-//   {
-//     path: '/map-vector',
-//     name: prefix + '.map-vector',
-//     meta: { auth: true, name: 'Vector Map', isBanner: true },
-//     component: () => import('@/views/maps/VectorPage.vue')
-//   },
-//   // Form Pages
-//   {
-//     path: '/elements',
-//     name: prefix + '.elements',
-//     meta: { auth: true, name: 'Elements', isBanner: true },
-//     component: () => import('@/views/forms/ElementsPage.vue')
-//   },
-//   {
-//     path: '/validation',
-//     name: prefix + '.validation',
-//     meta: { auth: true, name: 'Validation', isBanner: true },
-//     component: () => import('@/views/forms/ValidationPage.vue')
-//   },
-//   {
-//     path: '/wizard',
-//     name: prefix + '.wizard',
-//     meta: { auth: true, name: 'Wizard', isBanner: true },
-//     component: () => import('@/views/forms/WizardPage.vue')
-//   },
-//   // Table Pages
-//   {
-//     path: '/bootstrap-table',
-//     name: prefix + '.bootstrap-table',
-//     meta: { auth: true, name: 'Botstrap Table', isBanner: true },
-//     component: () => import('@/views/tables/BootstrapTable.vue')
-//   },
-//   {
-//     path: '/datatable',
-//     name: prefix + '.data-table',
-//     meta: { auth: true, name: 'Data Table', isBanner: true },
-//     component: () => import('@/views/tables/DataTable.vue')
-//   },
-//   // Icons Pages
-//   {
-//     path: '/icons/solid',
-//     name: prefix + '.icons.solid',
-//     meta: { auth: true, name: 'Solid Icon', isBanner: true },
-//     component: () => import('@/views/icons/SolidIcon.vue')
-//   },
-//   {
-//     path: '/icons/outlined',
-//     name: prefix + '.icons.outlined',
-//     meta: { auth: true, name: 'Outlined Icon', isBanner: true },
-//     component: () => import('@/views/icons/OutlinedIcon.vue')
-//   },
-//   {
-//     path: '/icons/dual-tone',
-//     name: prefix + '.icons.dual-tone',
-//     meta: { auth: true, name: 'Dual Tone Icon', isBanner: true },
-//     component: () => import('@/views/icons/DualToneIcon.vue')
-//   },
-//   // Extra Pages
-//   {
-//     path: '/privacy-policy',
-//     name: prefix + '.privacy-policy',
-//     meta: { auth: true, name: 'Privacy Policy', isBanner: true },
-//     component: () => import('@/views/extra/PrivacyPolicy.vue')
-//   },
-//   {
-//     path: '/terms-and-conditions',
-//     name: prefix + '.terms-and-conditions',
-//     meta: { auth: true, name: 'Terms and Conditions', isBanner: true },
-//     component: () => import('@/views/extra/TermsAndConditions.vue')
-//   },
-//   {
-//     path: '/admin-permissions',
-//     name: prefix + '.admin-permissions',
-//     meta: { auth: true, name: 'Admin Permissions', isBanner: true },
-//     component: () => import('@/views/admin/AdminPage.vue')
-//   }
+  // // Spacial Pages
+  // {
+  //   path: '/billing',
+  //   name: prefix + '.billing',
+  //   meta: { auth: true, name: 'Billing', isBanner: true },
+  //   component: () => import('@/views/spacial-pages/BillingPage.vue')
+  // },
+  // {
+  //   path: '/calender',
+  //   name: prefix + '.calender',
+  //   meta: { auth: true, name: 'Calender', isBanner: true },
+  //   component: () => import('@/views/spacial-pages/CalenderPage.vue')
+  // },
+  // {
+  //   path: '/kanban',
+  //   name: prefix + '.kanban',
+  //   meta: { auth: true, name: 'Kanban', isBanner: true },
+  //   component: () => import('@/views/spacial-pages/KanbanPage.vue')
+  // },
+  // {
+  //   path: '/pricing',
+  //   name: prefix + '.pricing',
+  //   meta: { auth: true, name: 'Pricing', isBanner: true },
+  //   component: () => import('@/views/spacial-pages/PricingPage.vue')
+  // },
+  // {
+  //   path: '/timeline',
+  //   name: prefix + '.timeline',
+  //   meta: { auth: true, name: 'Timeline', isBanner: true },
+  //   component: () => import('@/views/spacial-pages/TimelinePage.vue')
+  // },
+  // {
+  //   path: '/rtl-support',
+  //   name: prefix + '.rtlsupport',
+  //   meta: { auth: true, name: 'RTL-Support', isBanner: true },
+  //   component: () => import('@/views/spacial-pages/RtlSupport.vue')
+  // },
+  // // Users Pages
+  // {
+  //   path: '/user-list',
+  //   name: prefix + '.user-list',
+  //   meta: { auth: true, name: 'User List', isBanner: true },
+  //   component: () => import('@/views/user/ListPage.vue')
+  // },
+  // {
+  //   path: '/user-add',
+  //   name: prefix + '.user-add',
+  //   meta: { auth: true, name: 'User Add', isBanner: true },
+  //   component: () => import('@/views/user/AddPage.vue')
+  // },
+  // {
+  //   path: '/user-profile',
+  //   name: prefix + '.user-profile',
+  //   meta: { auth: true, name: 'User Add', isBanner: true },
+  //   component: () => import('@/views/user/ProfilePage.vue')
+  // },
+  // {
+  //   path: '/privacy-setting',
+  //   name: prefix + '.user-privacy-setting',
+  //   meta: { auth: true, name: 'Privacy Setting', isBanner: true },
+  //   component: () => import('@/views/user/PrivacySetting.vue')
+  // },
+  // // Widgets Pages
+  // {
+  //   path: '/widget-basic',
+  //   name: prefix + '.widget-basic',
+  //   meta: { auth: true, name: 'Widget Basic', isBanner: true },
+  //   component: () => import('@/views/widgets/WidgetBasic.vue')
+  // },
+  // {
+  //   path: '/widget-chart',
+  //   name: prefix + '.widget-chart',
+  //   meta: { auth: true, name: 'Widget Chart', isBanner: true },
+  //   component: () => import('@/views/widgets/WidgetChart.vue')
+  // },
+  // {
+  //   path: '/widget-card',
+  //   name: prefix + '.widget-card',
+  //   meta: { auth: true, name: 'Widget Card', isBanner: true },
+  //   component: () => import('@/views/widgets/WidgetCard.vue')
+  // },
+  // // Map Pages
+  // {
+  //   path: '/map-google',
+  //   name: prefix + '.map-google',
+  //   meta: { auth: true, name: 'Google Map', isBanner: true },
+  //   component: () => import('@/views/maps/GooglePage.vue')
+  // },
+  // {
+  //   path: '/map-vector',
+  //   name: prefix + '.map-vector',
+  //   meta: { auth: true, name: 'Vector Map', isBanner: true },
+  //   component: () => import('@/views/maps/VectorPage.vue')
+  // },
+  // // Form Pages
+  // {
+  //   path: '/elements',
+  //   name: prefix + '.elements',
+  //   meta: { auth: true, name: 'Elements', isBanner: true },
+  //   component: () => import('@/views/forms/ElementsPage.vue')
+  // },
+  // {
+  //   path: '/validation',
+  //   name: prefix + '.validation',
+  //   meta: { auth: true, name: 'Validation', isBanner: true },
+  //   component: () => import('@/views/forms/ValidationPage.vue')
+  // },
+  // {
+  //   path: '/wizard',
+  //   name: prefix + '.wizard',
+  //   meta: { auth: true, name: 'Wizard', isBanner: true },
+  //   component: () => import('@/views/forms/WizardPage.vue')
+  // },
+  // // Table Pages
+  // {
+  //   path: '/bootstrap-table',
+  //   name: prefix + '.bootstrap-table',
+  //   meta: { auth: true, name: 'Botstrap Table', isBanner: true },
+  //   component: () => import('@/views/tables/BootstrapTable.vue')
+  // },
+  // {
+  //   path: '/datatable',
+  //   name: prefix + '.data-table',
+  //   meta: { auth: true, name: 'Data Table', isBanner: true },
+  //   component: () => import('@/views/tables/DataTable.vue')
+  // },
+  // // Icons Pages
+  // {
+  //   path: '/icons/solid',
+  //   name: prefix + '.icons.solid',
+  //   meta: { auth: true, name: 'Solid Icon', isBanner: true },
+  //   component: () => import('@/views/icons/SolidIcon.vue')
+  // },
+  // {
+  //   path: '/icons/outlined',
+  //   name: prefix + '.icons.outlined',
+  //   meta: { auth: true, name: 'Outlined Icon', isBanner: true },
+  //   component: () => import('@/views/icons/OutlinedIcon.vue')
+  // },
+  // {
+  //   path: '/icons/dual-tone',
+  //   name: prefix + '.icons.dual-tone',
+  //   meta: { auth: true, name: 'Dual Tone Icon', isBanner: true },
+  //   component: () => import('@/views/icons/DualToneIcon.vue')
+  // },
+  // // Extra Pages
+  // {
+  //   path: '/privacy-policy',
+  //   name: prefix + '.privacy-policy',
+  //   meta: { auth: true, name: 'Privacy Policy', isBanner: true },
+  //   component: () => import('@/views/extra/PrivacyPolicy.vue')
+  // },
+  // {
+  //   path: '/terms-and-conditions',
+  //   name: prefix + '.terms-and-conditions',
+  //   meta: { auth: true, name: 'Terms and Conditions', isBanner: true },
+  //   component: () => import('@/views/extra/TermsAndConditions.vue')
+  // },
+  // {
+  //   path: '/admin-permissions',
+  //   name: prefix + '.admin-permissions',
+  //   meta: { auth: true, name: 'Admin Permissions', isBanner: true },
+  //   component: () => import('@/views/admin/AdminPage.vue')
+  // }
 ]
 
 // const errorRoutes = (prefix) => [
@@ -255,6 +255,16 @@ const routes = [
   //   component: () => import('../layouts/guest/BlankLayout.vue'),
   //   children: designSystemChildRoutes('design-system')
   // },
+  {
+    path: '/',
+    redirect: '/auth/login', // Redirect the root path to the sign-in page
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: () => import('../layouts/guest/BlankLayout.vue'),
+    children: authChildRoutes('auth')
+  },
   // Default Pages
   {
     path: '/dashboard',
@@ -301,16 +311,7 @@ const routes = [
   //   component: () => import('../layouts/guest/BlankLayout.vue'),
   //   children: authChildRoutes('auth')
   // },
-  {
-    path: '/',
-    redirect: '/auth/login', // Redirect the root path to the sign-in page
-  },
-  {
-    path: '/auth',
-    name: 'auth',
-    component: () => import('../layouts/guest/BlankLayout.vue'),
-    children: authChildRoutes('auth')
-  },
+
 
   // // Errors Pages
   // {
