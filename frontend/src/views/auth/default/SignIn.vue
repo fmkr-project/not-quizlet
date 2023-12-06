61801b5a2965e56e88090d7630f84b99ff32b644
rmnouv <rathanakmuny2nouv@gmail.com>
2023-12-06 22:26:03 +0100

    Create the logic function for the sign in template

--- frontend/src/views/auth/default/SignIn.vue
+++ frontend/src/views/auth/default/SignIn.vue
@@ -11,23 +11,23 @@
               </router-link>
               <h2 class="mb-2 text-center">Sign In</h2>
               <p class="text-center">Login to stay connected.</p>
-              <form>
+              <form @submit.prevent="toHomeDashboard">
                 <div class="row">
                   <div class="col-lg-12">
                     <div class="form-group">
                       <label for="email" class="form-label">Email</label>
-                      <input type="email" class="form-control" id="email" aria-describedby="email" placeholder=" " />
+                      <input type="email" class="form-control" id="email" v-model="email" aria-describedby="email" placeholder=" " />
                     </div>
                   </div>
                   <div class="col-lg-12">
                     <div class="form-group">
                       <label for="password" class="form-label">Password</label>
-                      <input type="password" class="form-control" id="password" aria-describedby="password" placeholder=" " />
+                      <input type="password" class="form-control" id="password" v-model="password" aria-describedby="password" placeholder=" " />
                     </div>
                   </div>
                   <div class="col-lg-12 d-flex justify-content-between">
                     <div class="form-check mb-3">
-                      <input type="checkbox" class="form-check-input" id="customCheck1" />
+                      <input type="checkbox" class="form-check-input" id="customCheck1" v-model="rememberMe" />
                       <label class="form-check-label" for="customCheck1">Remember Me</label>
                     </div>
                     <a href="/auth/reset-password">Forgot Password?</a>
@@ -36,21 +36,12 @@
                 <div class="d-flex justify-content-center">
                   <button type="submit" class="btn btn-primary">Sign In</button>
                 </div>
+                <p v-if="hasError" class="text-danger text-center">Invalid credentials. Please try again.</p>
                 <p class="mt-3 text-center">Don’t have an account? <a href="/auth/register" class="text-underline">Click here to sign up.</a></p>
               </form>
             </b-card>
           </b-col>
         </b-row>
-        <!--<div class="sign-bg">
-          <svg width="280" height="230" viewBox="0 0 431 398" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.05">
-              <rect x="-157.085" y="193.773" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 -157.085 193.773)" fill="#3B8AFF" />
-              <rect x="7.46875" y="358.327" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 7.46875 358.327)" fill="#3B8AFF" />
-              <rect x="61.9355" y="138.545" width="310.286" height="77.5714" rx="38.7857" transform="rotate(45 61.9355 138.545)" fill="#3B8AFF" />
-              <rect x="62.3154" y="-190.173" width="543" height="77.5714" rx="38.7857" transform="rotate(45 62.3154 -190.173)" fill="#3B8AFF" />
-            </g>
-          </svg>
-        </div>-->
       </b-col>
       <div class="col-md-6 d-md-block d-none bg-primary p-0 vh-100 overflow-hidden">
         <img src="@/assets/images/auth/01.png" class="img-fluid gradient-main animated-scaleX" alt="images" loading="lazy" />
@@ -59,61 +50,40 @@
   </section>
 </template>
 
-<script>
-import { useRouter } from "vue-router";
-import { ref } from "vue";
-import axios from "axios";
+<script setup>
+  import { ref } from "vue";
+  import axios from "axios";
+  import { useRouter } from "vue-router";
 
-export default {
-  name: 'LoginComponent',
-  setup() {
-    const router = useRouter();
-    const email = ref('');
-    const password = ref('');
-    const hasError = ref(false);
-    const rememberMe = ref(false);
+  const email = ref('');
+  const password = ref('');
+  const rememberMe = ref(false);
+  const hasError = ref(false);
 
-    const toHomeDashboard = async () => {
-      try {
-        const response = await axios.post("http://127.0.0.1:5001/api/login", {
-          email: email.value,
-          password: password.value
-        });
+  const router = useRouter();
 
-        if (response.data.user.email === email.value) {
-          // Authentication successful
-          localStorage.setItem("token", response.data.user.token);
-          console.log("Redirecting to the dashboard");
+  const toHomeDashboard = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:5001/api/users/login", {
+        email: email.value,
+        password: password.value
+      });
 
-          if (rememberMe.value) {
-            localStorage.setItem("rememberMe", "true");
-          } else {
-            localStorage.removeItem("rememberMe");
-          }
-
-          router.push({ name: "default.dashboard" });
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        if (rememberMe.value) {
+          localStorage.setItem("rememberMe", "true");
         } else {
-          // Authentication failed
-          hasError.value = true;
-          console.error("Invalid credentials");
+          localStorage.removeItem("rememberMe");
         }
-      } catch (error) {
+        router.push({ name: "default.dashboard" });
+      } else {
         hasError.value = true;
-        console.error("Error:", error);
       }
-    };
-
-    // Return the reactive properties and methods to use in the template
-    return {
-      email,
-      password,
-      hasError,
-      rememberMe,
-      toHomeDashboard
-    };
-  }
-};
+    } catch (error) {
+      hasError.value = true;
+    }
+  };
 </script>
-
 
 <style lang="scss" scoped></style>
