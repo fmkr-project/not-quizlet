c392a0ccfeedbd9c8b0697fd7d28edc8cefdae59
rmnouv <rathanakmuny2nouv@gmail.com>
2023-12-06 23:14:22 +0100

    SignIn

--- frontend/src/views/auth/default/SignIn.vue
+++ frontend/src/views/auth/default/SignIn.vue
@@ -82,6 +82,20 @@
       }
     } catch (error) {
       hasError.value = true;
+      console.error('Login error:', error);
+      if (error.response) {
+        // The server responded with a status code that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
     }
   };
 </script>
