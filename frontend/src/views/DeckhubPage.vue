ee8bdf4bd7d7ec0e690b1aa896a4755feb320f66
rmnouv <rathanakmuny2nouv@gmail.com>
2023-12-21 00:59:23 +0100

    Create a page for deckhub

---
+++ frontend/src/views/DeckhubPage.vue
@@ -0,0 +1,23 @@
+<script>
+import HeaderComponent from '@/components/layout/HeaderComponent.vue';
+import DeckhubComponent from '@/components/pages/DeckhubComponent.vue'
+import FooterComponent from '@/components/layout/FooterComponent.vue';
+export default {
+    components: {
+        HeaderComponent,
+        DeckhubComponent,
+        FooterComponent
+    }
+    // other options
+};
+</script>
+<template>
+  <body>
+    <HeaderComponent/>
+    <DeckhubComponent/>
+    <FooterComponent/>
+  </body>
+  <!-- Rest of your homepage content -->
+</template>
+<style scoped>
+</style>
