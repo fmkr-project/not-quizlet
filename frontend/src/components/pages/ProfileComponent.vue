9330cce590a5c1be9ee946d2c77f707bfd71b05e
Xichen <108240415+SeasonXC@users.noreply.github.com>
2023-12-20 17:52:51 +0000

    profile component

---
+++ frontend/src/components/pages/ProfileComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="profile-container">
+    <div class="profile-picture">
+      <label for="profile-upload">Upload Profile Picture:</label>
+      <input type="file" id="profile-upload" @change="onFileChange">
+    </div>
+    <div class="change-password">
+      <button @click="onChangePasswordClick">Change Password</button>
+    </div>
+    <div class="change-username">
+      <label for="username">New Username:</label>
+      <input type="text" id="username">
+      <button @click="onChangeUsernameClick">Change Username</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  methods: {
+    onFileChange() {
+      // Handle file upload
+    },
+    onChangePasswordClick() {
+      // Handle password change
+    },
+    onChangeUsernameClick() {
+      // Handle username change
+    }
+  }
+}
+</script>
+
+<style scoped>
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+}
+
+.profile-picture input {
+  margin-top: 10px;
+}
+
+.change-password, .change-username {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.change-username input {
+  margin-bottom: 10px;
+}
+</style>
