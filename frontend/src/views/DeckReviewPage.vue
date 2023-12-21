aa1608d9a3d56e7bcb3e83588fb322c23b960896
rmnouv <rathanakmuny2nouv@gmail.com>
2023-12-21 04:54:21 +0100

    Create a page for review card

---
+++ frontend/src/views/DeckReviewPage.vue
@@ -0,0 +1,23 @@
+<script>
+import HeaderComponent from '@/components/layout/HeaderComponent.vue';
+import DeckReviewComponent from '@/components/pages/DeckReviewComponent.vue'
+import FooterComponent from '@/components/layout/FooterComponent.vue';
+export default {
+    components: {
+        HeaderComponent,
+        DeckReviewComponent,
+        FooterComponent
+    }
+    // other options
+};
+</script>
+<template>
+  <body>
+    <HeaderComponent/>
+    <DeckReviewComponent/>
+    <FooterComponent/>
+  </body>
+  <!-- Rest of your homepage content -->
+</template>
+<style scoped>
+</style>
